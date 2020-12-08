8faa24710108f0d2f9565f6ce1e2fc8cccf78c0a
Katherine Lamoureux <katherine.lamoureux@gmail.com>
2020-12-08 11:39:25 -0500

    Create RandomSelection.cs

---
+++ RandomSelection.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+namespace Demo {
+   class Program {
+      static void Main(string[] args) {
+         var random = new Random();
+         var list = new List<string>{ "one","two","three","four"};
+         int index = random.Next(list.Count);
+         Console.WriteLine(list[index]);
+      }
+   }
+}
